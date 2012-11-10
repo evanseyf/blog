64851f7e2ebd8dbefef365fb4857c8e277035c52
Conor <conorsg@gmail.com>
2012-11-09 20:17:02 -0600

    full for push

--- scripts/default.js
+++ scripts/default.js
@@ -1,15 +1,4 @@
 (function ($, win, doc) {
-	var slug = window.location.href.split('.com')[1];
 
-	function init() {
-		var navItem = $('nav').find('a');
-		$.each(navItem, function() {
-			if(navItem === slug) {
-				$(this).addClass('active');
-			} else {}
-		})
-	}
-
-	init();
 
 })(jQuery, window, document);
