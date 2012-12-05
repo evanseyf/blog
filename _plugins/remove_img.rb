bbab9717814852221debd657a0e1e911e2f1f594
Conor Gaffney <conorsg@gmail.com>
2012-12-05 12:16:55 -0500

    remove image plugin and implementation in index

---
+++ _plugins/remove_img.rb
@@ -0,0 +1,19 @@
+# pop all images into array and then exact match which one(s) to keep
+# made by Conor Gaffney, 2012
+
+module Jekyll
+	module RemoveImage
+  		def remove_img(input, keep = '')
+      		img_array = input.scan(/<img.*?>/)
+    		keep_last = img_array.count - keep.to_i
+    		img_array = img_array.last(keep_last)
+
+    		img_array.each do |x|
+    			input = input.gsub(/#{x}/, '')
+    		end
+    		input
+    	end
+  	end
+end
+
+Liquid::Template.register_filter(Jekyll::RemoveImage)
